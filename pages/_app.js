8511cf83c5dc182d0779092e9242f4aaf2899469
UjjwalThakur065 <ujjwaltha065@gmail.com>
2022-09-07 12:19:41 +0530

    My frontend app for web3rsvp project

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+
+const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
+
+const { chains, provider } = configureChains(
+  [chain.goerli],
+  [alchemyProvider({ alchemyId }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "web3rsvp",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains}>
+      <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      </ApolloProvider>
+    </RainbowKitProvider>
+  </WagmiConfig>
+  );
+}
---
+++ utils/connectContract.js
@@ -0,0 +1,27 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+
+function connectContract() {
+    //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
+    const contractAddress = "0x9CB5DB936F2d1cF6A69edbAeaC716857b982c680";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
